cbcc34464111830f630b63871acf18279c6eb153
ViktoriiaHavryliuk <havriliuk89@gmail.com>
2014-08-14 17:43:26 +0300

    Update Person.java

--- src/com/epam/task4/Person.java
+++ src/com/epam/task4/Person.java
@@ -6,28 +6,53 @@ package com.epam.task4;
  */
 public class Person {
 
-	private String firstName;
-	private String lastName;
-	private int dateOfBirth;
-	
-	public Person ( String firstName, String lastName, int dateOfBirth){
-		this.firstName = firstName;
-		this.lastName = lastName;
+	private String firstName; 
+	private String lastName; 
+	private int dateOfBirth; 
+	private long id ;
+	Person boss;
+	Person[] collegues;
+	private static final int MIN_DATE_OF_BIRTH = 1900;
+
+	static long counter = 0;
+
+	{
+		id = counter++;
+	}
+
+	public Person ( String firstName, String lastName, int dateOfBirth){ 
+		this.firstName = firstName; 
+		this.lastName = lastName; 
 		this.dateOfBirth = dateOfBirth;
+		if (dateOfBirth < MIN_DATE_OF_BIRTH) {
+			throw new IllegalArgumentException(
+					"Date of birth must be >= " + MIN_DATE_OF_BIRTH);
+		}
 	}
-	
-	public String getFullName(){
-		return firstName + " " + lastName;
+
+	public Person (long id, Person boss){
+		this.id = id;
+		this.boss = boss;
 	}
+
+	public String getFullName(){ 
+		return firstName + " " + lastName; 
+	} 
+
+	public int getAge(int curretnYear){ 
+		return curretnYear - dateOfBirth; 
+	} 
 	
-	public int getAge(int curretnYear){
-		return curretnYear - dateOfBirth;
+	public long getId(){
+		return id;
 	}
-	
-	@Override
-    public String toString() {
-        return firstName + " " + lastName;
-    }
-	
-	
+
+	static long getTotalNumber() {
+		return counter;
+	}
+
+	@Override 
+	public String toString() { 
+		return firstName + " " + lastName; 
+	} 
 }
